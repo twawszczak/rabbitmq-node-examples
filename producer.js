295342fe68242420177e737b5ef426f90763f1df
Tadeusz Wawszczak <tadeusz.wawszczak@onet.eu>
2017-08-09 06:03:34 -0400

    improve connections management, some other fixes

--- producer.js
+++ producer.js
@@ -2,20 +2,12 @@ const Producer = require('./lib/Producer')
 const ProducerOptions = require('./lib/ProducerOptions')
 
 const producerOptions = new ProducerOptions({
-  appName: 'mq-producer',
-  amqpUrl: 'amqp://localhost',
-  queueName: 'incoming',
-  durable: true,
-  dlx: 'deferred',
-  messageSize: 1,
-  messageContent: 'A',
-  repeats: 1,
-  logLevel: 'debug'
-}, {}, process.argv.slice(2));
+  isLoggerColored: process.stdout.isTTY
+}, process.argv.slice(2));
 
-(async () => {
+(module.exports.run = async () => {
   const producer = await new Producer(producerOptions)
-  producer.createLogger(producerOptions.appName, producerOptions.logLevel, process.stdout.isTTY)
+  producer.createLogger(producerOptions.appName, producerOptions.logLevel)
   await producer.connect()
   await producer.produce()
   producer.disconnect()
